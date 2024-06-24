a0a4d779ffe53132c5c531a33ecd7f31db499d1f
eugene <eugene.dbv@gmail.com>
2024-06-24 12:08:16 +0300

    delete * in import

--- app/src/test/java/hexlet/code/NumberSchemaTest.java
+++ app/src/test/java/hexlet/code/NumberSchemaTest.java
@@ -2,7 +2,9 @@ package hexlet.code;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NumberSchemaTest {
     private Validator validator;
