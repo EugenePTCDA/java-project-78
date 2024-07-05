bcb52a4418e5bef3317a09e3620bc8f460a0ebf0
eugene <eugene.dbv@gmail.com>
2024-07-05 10:57:58 +0300

    change HashMap to LinkedHashMap

--- app/src/main/java/hexlet/code/schemas/BaseSchema.java
+++ app/src/main/java/hexlet/code/schemas/BaseSchema.java
@@ -1,11 +1,11 @@
 package hexlet.code.schemas;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.function.Predicate;
 
 public abstract class BaseSchema<T> {
-    protected Map<String, Predicate<T>> validators = new HashMap<>();
+    protected Map<String, Predicate<T>> validators = new LinkedHashMap<>();
 
     public final void addValidator(String key, Predicate<T> validator) {
         validators.put(key, validator);
